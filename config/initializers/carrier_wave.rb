8f75fa7c139fd63f8126d5d212b7641cbd083490
jureva <ejure@ymail.com>
2016-11-20 22:50:21 +0000

    additional settings for amazon

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -5,9 +5,7 @@ if Rails.env.production?
       :provider              => 'AWS',
       :aws_access_key_id     => ENV['S3_ACCESS_KEY'],
       :aws_secret_access_key => ENV['S3_SECRET_KEY'],
-      :s3_host_name => 's3-eu-west-1.amazonaws.com',
-      :s3_protocol => 'https',
-      :s3_permission => :private
+      :region => ENV['S3_REGION']
     }
     config.fog_directory     =  ENV['S3_BUCKET']
   end
